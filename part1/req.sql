b585159c03241d976c5abb458be532d94ede168b
Aurellis <62498557+Aureliona1@users.noreply.github.com>
2025-06-20 07:19:32 +0000

    [automatic commit] 2025-06-20 07:19:32 UTC 129.127.202.35

--- part1/req.sql
+++ part1/req.sql
@@ -1,2 +1,2 @@
-USE 
+USE DogWalkService;
 SELECT Users.username AS walker_username, COUNT(WalkRatings.rating_id) AS total_ratings, AVG(WalkRatings.rating) AS average_rating, COUNT(WalkRequests.request_id) AS completed_walks FROM ((Users LEFT JOIN WalkRatings ON WalkRatings.walker_id = Users.user_id) LEFT JOIN WalkRequests ON WalkRequests.request_id = WalkRatings.request_id AND WalkRequests.status = 'completed') WHERE Users.role = 'walker' GROUP BY Users.username;
