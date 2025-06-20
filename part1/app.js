2b13c8f58ba233e5f5be7a8d36c85ebbe823ec7e
Aurellis <62498557+Aureliona1@users.noreply.github.com>
2025-06-20 07:10:36 +0000

    [automatic commit] 2025-06-20 07:10:35 UTC 129.127.202.35

--- part1/app.js
+++ part1/app.js
@@ -134,7 +134,8 @@ app.get("/api/walkrequests/open", async (_, res) => {
 app.get("/api/walkers/summary", async (_, res) => {
   try {
     /*
-    Quick block here so I can 
+    Quick block here for constructing the request:
+    SELECT column names with
     */
     // This is an absolute monster of a request
     const [rows] = await db.execute(`
